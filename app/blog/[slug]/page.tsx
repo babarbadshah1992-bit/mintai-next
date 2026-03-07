f77e1f1221d0f75376103f52af3a9bf6cd8e5b1b
MD BABAR <babarbadshah1992@gmail.com>
2026-03-07 23:03:01 +0530

    remove createClient

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,28 +3,23 @@ import { supabase } from "../../../lib/supabase";
 
 export default async function Home() {
 
-  const supabase = createClient();
+const { data: blogs } = await supabase
+.from("blogs")
+.select("*")
+.order("created_at", { ascending: false });
 
-  const { data: blogs } = await supabase
-    .from("blogs")
-    .select("*")
-    .order("created_at", { ascending: false });
+return (
+<div style={{ maxWidth: "800px", margin: "50px auto" }}>
+<h1>MintAI Health Blogs</h1>
 
-  return (
-    <div style={{ maxWidth: "800px", margin: "50px auto" }}>
-      <h1>MintAI Health Blogs</h1>
+{blogs?.map((blog) => (
+<div key={blog.id}>
+<Link href={`/blog/${blog.slug}`}>
+<h2>{blog.title}</h2>
+</Link>
+</div>
+))}
 
-      {blogs?.map((blog) => (
-        <div key={blog.id} style={{ marginTop: "20px" }}>
-          <Link href={`/blog/${blog.slug}`}>
-            <h2 style={{ cursor: "pointer", color: "blue" }}>
-              {blog.title}
-            </h2>
-          </Link>
-          <p>{blog.content}</p>
-        </div>
-      ))}
-
-    </div>
-  );
+</div>
+);
 }
