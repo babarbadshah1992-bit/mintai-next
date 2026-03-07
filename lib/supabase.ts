926388f3085eb15eb695cfa7dc5ca3cf75c92874
MD BABAR <babarbadshah1992@gmail.com>
2026-03-07 22:42:57 +0530

    fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,6 @@
-import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-export const createClient = () => {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-  return createSupabaseClient(supabaseUrl, supabaseKey);
-};
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
