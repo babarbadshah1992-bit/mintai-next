d0a51b7cec6ffc342c3053ed451b497106d73ffa
MD BABAR <babarbadshah1992@gmail.com>
2026-04-19 23:27:23 +0530

    add blog detail page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { supabase } from '../../../lib/supabase'
 import { notFound } from 'next/navigation'
+import Link from 'next/link'
 
 export default async function BlogPost({ params }: { params: { slug: string } }) {
   const { data: blog } = await supabase
@@ -13,12 +14,13 @@ export default async function BlogPost({ params }: { params: { slug: string } })
   return (
     <article>
       <h1>{blog.title}</h1>
-      <div className="tags" style={{ margin: '1rem 0' }}>
+      <div className="tags">
         {blog.tags?.map((tag: string) => (
           <span key={tag} className="tag">#{tag}</span>
         ))}
       </div>
       <div dangerouslySetInnerHTML={{ __html: blog.content }} />
+      <Link href="/blog">← Back to all blogs</Link>
     </article>
   )
 }
