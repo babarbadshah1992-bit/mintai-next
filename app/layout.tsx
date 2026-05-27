7cbd0e1fa4e7ff4a0637da922580bcc921602af4
MD BABAR <babarbadshah1992@gmail.com>
2026-05-27 16:44:37 +0530

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 ﻿import './globals.css';
 import Link from 'next/link';
+import Script from "next/script";
 import { Inter, Poppins } from 'next/font/google';
 
 const inter = Inter({
@@ -276,6 +277,19 @@ export default function RootLayout({
             </div>
           </div>
         </footer>
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-PY29ZCEGB1"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-PY29ZCEGB1');
+  `}
+</Script>
       </body>
     </html>
   );
